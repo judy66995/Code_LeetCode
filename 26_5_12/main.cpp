164961cf4dc3a5dd4e6682b37d3dfafa3d124ff1
judy66995 <2049936615@qq.com>
2026-05-13 00:38:55 +0800

    finish:添加一些解释

--- 26_5_12/main.cpp
+++ 26_5_12/main.cpp
@@ -8,18 +8,18 @@ public:
         int i = 0, j = 0;
         int n1 = version1.size(), n2 = version2.size();
 
-        while (i < n1 || j < n2) {
+        while (i < n1 || j < n2) {//  当 version1 和 version2 都没有比较完时，继续比较
             int num1 = 0, num2 = 0;
 
             // 解析 version1 当前段
-            while (i < n1 && version1[i] != '.') {
+            while (i < n1 && version1[i] != '.') {// 解析 version1 当前段，直到遇到 '.' 或者字符串结束
                 num1 = num1 * 10 + (version1[i] - '0');
                 i++;
             }
             i++; // 跳过 .
 
             // 解析 version2 当前段
-            while (j < n2 && version2[j] != '.') {
+            while (j < n2 && version2[j] != '.') {// 解析 version2 当前段，直到遇到 '.' 或者字符串结束
                 num2 = num2 * 10 + (version2[j] - '0');
                 j++;
             }
